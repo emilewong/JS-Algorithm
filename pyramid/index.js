e45c8dfaadb8591fdcb0b844ed4592956f95ab75
emilewong <me@emilewong.com>
2019-08-19 20:31:37 +0800

    Add 2 solutions to pyramid problem.

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,40 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+    if (row === n) {
+        return;
+    }
+
+    if (level.length === 2 * n - 1) {
+        console.log(level);
+        return pyramid(n, row + 1);
+    }
+
+    const midpoint = Math.floor((n * 2 - 1) / 2);
+    if (midpoint - row <= level.length && midpoint + row >= level.length) {
+        level += '#';
+    } else {
+        level += ' ';
+    }
+
+    pyramid(n, row, level);
+}
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//     for (let row = 0; row < n; row++) {
+//         let level = "";
+
+//         for (let column = 0; column < (n * 2 - 1); column++) {
+//             const midpoint = Math.floor((n * 2 - 1) / 2);
+//             if (midpoint - row <= column && midpoint + row >= column) {
+//                 level += "#";
+//             } else {
+//                 level += " ";
+//             }
+//         }
+//         console.log(level);
+//     }
+// }
