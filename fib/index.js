d1642a60668c71611d2de0a4447a8cd08a4b2637
emilewong <me@emilewong.com>
2019-08-20 16:17:58 +0800

    Added a memorization cache function to speedup recursive Fibonacci solution.

--- fib/index.js
+++ fib/index.js
@@ -8,16 +8,28 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-    const result = [0, 1];
-    for (let i = 2; i <= n; i++) {
-        const a = result[i - 1];
-        const b = result[i - 2];
-        result.push(a + b);
-    }
-    return result[n];
+function memorize(fn) {
+    const cache = {};
+    return function (...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    };
 }
 
+function slowFib(n) {
+    if (n < 2) {
+        return n;
+    }
+    return slowFib(n - 1) + slowFib(n - 2);
+}
+
+const fib = memorize(slowFib);
 
 module.exports = fib;
 
@@ -26,4 +38,14 @@ module.exports = fib;
 //         return n;
 //     }
 //     return fib(n - 1) + fib(n - 2);
+// }
+
+// function fib(n) {
+//     const result = [0, 1];
+//     for (let i = 2; i <= n; i++) {
+//         const a = result[i - 1];
+//         const b = result[i - 2];
+//         result.push(a + b);
+//     }
+//     return result[n];
 // }
