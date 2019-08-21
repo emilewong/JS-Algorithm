9808fddee5828fd48c0c73bb5af5ee314c5ff9f9
emilewong <me@emilewong.com>
2019-08-21 17:59:30 +0800

    Add solution of level width problem in Tree.

--- levelwidth/index.js
+++ levelwidth/index.js
@@ -11,6 +11,23 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const arr = [root, 'end'];
+    counters = [0];
+
+    while (arr.length > 1) {
+        const node = arr.shift();
+
+        if (node === 'end') {
+            counters.push(0);
+            arr.push('end');
+        } else {
+            arr.push(...node.children);
+            counters[counters.length - 1]++;
+        }
+    }
+
+    return counters;
+}
 
 module.exports = levelWidth;
